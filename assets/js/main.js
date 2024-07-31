5d24487846b6262cf7546fb86f65082bb6e927bd
izabelydev <izabelylrnc@gmail.com>
2024-07-30 22:17:15 -0300

    Remoção do vídeo e implementação da caixa "gostos pessoais"

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,9 @@
 const menu = document.querySelector('.navbar');
 const btnOpenMenu = document.querySelector('.menu-icon');
 const btnCloseMenu = document.querySelector('.arrow-back');
-const tech = document.querySelector('#technology');
-const openBoxVideo = document.querySelector('.btn-open-box-video');
+const certificates = document.querySelector('#certificates');
+const openBox = document.querySelectorAll('.btn-open-box');
 const a = document.getElementsByClassName('item');
-
-// alert('Esse projeto ainda não está 100% finalizado!');
 
 btnOpenMenu.addEventListener('click', () => {
     menu.classList.add('active');
@@ -18,24 +16,67 @@ btnCloseMenu.addEventListener('click', () => {
 for (let i = 0; i < a.length; i++) {
     a[i].addEventListener('click', () => {
         menu.classList.remove('active');
+        if (a[i].classList.contains('personal')) openPersonal();
     });
 }
 
-openBoxVideo.addEventListener('click', () => {
-    tech.innerHTML += `
-            <div class="wrapper-resume off" style="backdrop-filter: blur(5px);">
-                <div class="container-resume">
-                    <span class="close"><p>X</p></span>
-                    <div class="content-container">
-                        <p>No vídeo abaixo, vou te explicar o que me motivou a estudar Desenvolvimento Web, por que escolhi HTML, CSS, JS, Git, TS e Angular e quais as próximas tecnologias que pretendo estudar.</p>
-                        <video autoplay controls>
-                            <source src="./assets/video/tecnologias.mp4" type="video/mp4">
-                        </video>
+for (let i = 0; i < openBox.length; i++) {
+    openBox[i].addEventListener('click', event => {
+        const e = event.target;
+        if (e.classList.contains('personal')) openPersonal();
+    });
+}
+
+function openPersonal() {
+    const wrapperResume = document.createElement('div');
+    wrapperResume.classList.add('wrapper-resume', 'off');
+    wrapperResume.style.backdropFilter = 'blur(5px)';
+
+    wrapperResume.innerHTML = `
+        <div class="container-resume">
+            <span class="close"><a>X</a></span>
+            <div class="content-container">
+                <h1>Conheça meus gostos pessoais</h1>
+
+                <div class="personal-tastes">
+                    <div class="personal-taste">
+                        <img src="./assets/img/dog.png" alt="Cachorro">
+                        <p>Cuidar do cachorro</p>
+                    </div>
+    
+                    <div class="personal-taste">
+                        <img src="./assets/img/plant.png" alt="Natureza">
+                        <p>Cuidar da natureza</p>
+                    </div>
+    
+                    <div class="personal-taste">
+                        <img src="./assets/img/book.png" alt="Clube do livro">
+                        <p>Participar do clube do livro</p>
+                    </div>
+
+                    <div class="personal-taste">
+                        <img src="./assets/img/jesus.png" alt="Jesus">
+                        <p>Ir à igreja</p>
+                    </div>
+                    
+                    <div class="personal-taste">
+                        <img src="./assets/img/friendship.png" alt="Amigos e família">
+                        <p>Visitar família e amigos</p>
+                    </div>
+    
+                    <div class="personal-taste">
+                        <img src="./assets/img/watching-a-movie.png" alt="Assistir">
+                        <p>Assistir séries e filmes</p>
                     </div>
                 </div>
-            </div>`;
+            </div>
+        </div>`;
 
+    certificates.appendChild(wrapperResume);
+    animationBox();
+}
 
+function animationBox() {
     const wrapperResume = document.querySelector('.wrapper-resume');
     const btnCloseResume = document.querySelector('.close');
 
@@ -46,7 +87,7 @@ openBoxVideo.addEventListener('click', () => {
     btnCloseResume.addEventListener('click', () => {
         closeBoxAnim();
         setTimeout(() => {
-            tech.removeChild(wrapperResume);
+            certificates.removeChild(wrapperResume);
         }, 600);
     });
 
@@ -54,10 +95,9 @@ openBoxVideo.addEventListener('click', () => {
         wrapperResume.classList.remove('off');
         wrapperResume.classList.add('on');
     };
-    
-    
+
     function closeBoxAnim() {
         wrapperResume.classList.remove('on');
         wrapperResume.classList.add('off');
     };
-});
+}
